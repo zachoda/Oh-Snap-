87d1022a03fb42a5e70657e0766541606b2b5122
Zach Oda <zjodachow@gmail.com>
2022-10-12 16:05:29 -0400

    nav and about tested

---
+++ photo-port/src/Components/About/__tests__/index.test.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {render, cleanup} from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import About from '..';
+
+afterEach(cleanup);
+describe('About component', () => {
+    //renders About test
+
+    //First test
+    it('renders', () => {
+        render(<About/>);
+    });
+
+    //Second test
+    it('matches snapshot DOM node structure', () => {
+        const { asFragment } = render(<About />);
+        
+        expect(asFragment()).toMatchSnapshot();
+      });
+    });
