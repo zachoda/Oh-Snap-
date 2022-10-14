f81df2c1b5e723856b54dc3de4c34078b586e2bf
Zach Oda <zjodachow@gmail.com>
2022-10-14 12:40:16 -0400

    fixed photo render

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import About from './Components/About';
-import Nav from './Components/Nav';
-
+import React, { useState } from "react";
+import Nav from "./Components/Nav";
+import About from "./Components/About";
+import Gallery from "./Components/Gallery";
 
 function App() {
+  const [categories] = useState([
+    {
+      name: "commercial",
+      description:
+        "Photos of grocery stores, food trucks, and other commercial projects",
+    },
+    { name: "portraits", description: "Portraits of people in my life" },
+    { name: "food", description: "Delicious delicacies" },
+    {
+      name: "landscape",
+      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
+    },
+  ]);
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
+
   return (
     <div>
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
       <main>
-        <Nav></Nav>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
     </div>
